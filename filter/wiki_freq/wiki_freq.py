abe915136fc341aa06d7afaa6c80dfe3f8bcd650
Alen Kubati <kubatial@gmail.com>
2013-03-14 22:53:46 -0400

    Fiddling with the wiki filter

--- filter/wiki_freq/wiki_freq.py
+++ filter/wiki_freq/wiki_freq.py
@@ -1,12 +1,20 @@
 import wikipedia
 import wiki2plain
+import sys
+sys.path.append('../')
+import word_freq_dictionary as w
 
 class WikiFreq:
-	d = {}
-	total = 0
+#	d = {}
+	total = 0.0
 	alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+	eng_frequency = w.EnglishFrequency()
+	threshold = 1000000
+
 	def __init__(self, title):
-		lang = 'simple'
+		self.filt = []
+		self.d = {}
+		lang = 'en'
 		wiki = wikipedia.Wikipedia(lang)
 		r = ''
 		try:
@@ -17,6 +25,7 @@ class WikiFreq:
 			w = wiki2plain.Wiki2Plain(r)
 			content = w.text
 			self.calc_freq(content)
+			self.filt = self.generate_list()
 
 	def calc_freq(self, text):
 		lst = text.split()
@@ -35,4 +44,20 @@ class WikiFreq:
 					self.d[i] = 1
 				self.total += 1
 		print self.total
+
+	def generate_list(self):
+		lst = self.d.keys()
+		ret = []
+		for i in lst:
+			temp = 0
+			try:
+				temp = self.eng_frequency.get_freq(i.lower())
+			except:
+				temp = 0 #Word not in the english language
+			if self.d[i] / self.total >= self.threshold * temp:
+				ret = ret + [i]
+#		ret.remove('Wikia')
+#		ret.remove('website')
+#		ret.remove('websites')
+		return ret
 
