677bc54d29fb8c7261822fd435cdc1ac5892d722
Kent Chen <chkche1@gmail.com>
2013-04-09 18:17:33 -0400

    reimplemented ranker with a more workable interface

--- ranker.py
+++ ranker.py
@@ -1,120 +1,111 @@
-#!/usr/bin/env python
+import sys
+import os
+import fnmatch
+import operator
+import glob
+import cPickle as pickle
+from collections import defaultdict
 
-"""
+def recursive_glob(rootdir='.', pattern='*'):
+    return [os.path.join(rootdir, filename)
+            for rootdir, dirnames, filenames in os.walk(rootdir)
+            for filename in filenames
+            if fnmatch.fnmatch(filename, pattern)]
 
-The sentiment analysis module that computes polarity and intensity for each
-tweet. Then, book ranks are computed based on the total sum of the tweet
-scores. 
-
-"""
-__author__ = "Kent Chen"
-__copyright__ = "Copyright 2012, Kent Chen"
-__credits__ = []
-__license__ = ""
-__version__ = "0.1"
-__maintainer__ = "Kent Chen"
-__email__ = "kche@seas.upenn.edu"
-
-import sentiwordnet
-import tweet
-from nltk.tag.stanford import POSTagger
-
-# Hard coded path. Get all this into a config file in the future
-tagger_path = '/Users/kent/twit-ranker/wsj-0-18-bidirectional-distsim.tagger'
-tagger_jar_path = '/Users/kent/twit-ranker/stanford-postagger-3.1.4.jar'
-
-# Lookup table. 'q' indicates that the pos should not get a score
-
-pos_table = {'JJ': 'a'\
-		'JJR': 'a'\
-		'JJS': 'a'\
-		'NN': 'n'\
-		'NNP': 'n'\
-		'NNPS': 'n'\
-		'NNS': 'n'\
-		'RB': 'r'\
-		'RBR': 'r'\
-		'RBS': 'r'\
-		'VB':'v'\
-		'VBD':'v'\
-		'VBG':'v'\
-		'VBN':'v'\
-		'VBP':'v'\
-		'VBZ':'v'
-		}
+def rank_by_aggregate_pmi(folders):
+	book_pmi = defaultdict(int)
+	for folder in folders:
+		scores = [x for x in recursive_glob(folder,'*.score')]
+		for filename in scores:
+			l = pickle.load(open(filename,'rb'))
+			for book in l.keys():
+				book_pmi[book] += sum(l[book]['pmi'])
+	sorted_x = sorted(book_pmi.iteritems(),
+		key=operator.itemgetter(1))
+	sorted_x.reverse()
+	return [x[0] for x in sorted_x]
 
 
-class Ranker:
-	def __init__(self, filename):
-		"""
-		Argument:
-		filename -- path to the SentiWordNet file
-		"""
-		self.swn = SentiWordNetCorpusReader(filename);
-		self.st = POSTagger(tagger_path, tagger_jar_path)
-	
-	def compute_score(tweets):
-		"""
-		Argument:
-		tweets -- compute the tweet score for the input batch of tweets
-		"""
-		running_sum = 0
-		for tweet in tweets:
-			score, text = _evaluate_emoticons(tweet.text)
-			pos = self.st.tag(text.split())
-			for p in pos:
-				val = _lookup_pos(p[1])
-				if val == 'q':
-					continue
-				
-				temp = self.swn.senti_synsets(p[0], val)
-				for word in temp:
-					score[0] = word.pos_score
-					score[1] = word.neg_score
-			if score[0] > score[1]:
-				running_sum += score[0]:
+def get_stat(d):
+	sortedkeys = sorted(d, key=lambda key: int(key.split('k')[1]))
+	for k in sortedkeys:
+		if 'avg_sent' not in d[k] or 'rank' not in d[k]:
+			continue
+		else:
+			print '++++++++++++++++'
+			print k
+			print 'rank: ', d[k]['rank']
+			print 'average sentiment: ',d[k]['avg_sent']
+			print 'cue: ', d[k]['cue']
+			print 'p_cue: ', d[k]['p_cue']
+			print 'n_cue: ', d[k]['n_cue']
+			print 'emo: ', d[k]['emo']
+			print 'pmi: ', d[k]['pmi']
+			print '++++++++++++++++'
+
+
+# For every book, map week -> {rank, all kinds of stats}
+
+if __name__ =='__main__':
+	arg = sys.argv[1:]
+	#print rank_by_aggregate_pmi(arg)
+
+	ranks = defaultdict(lambda : defaultdict(dict))
+	booklists = glob.glob('./nyt-lists/*.bkl')
+
+	for bl in booklists:
+		week = os.path.basename(bl).split('.')[0]
+		with open(bl, 'rU') as f:
+			for idx, line in enumerate(f):
+				title = line.split('|')[0].lower().replace(' ','_')
+				ranks[title][week]['rank'] = idx+1
+
+	scores = glob.glob('./scores/*.score')
+
+	#for folder in arg:
+		#scores = [x for x in recursive_glob(folder,'*.score')]
+	for filename in scores:
+		basename = os.path.basename(filename)
+		week = basename.split('.')[0]
+		l = pickle.load(open(filename,'rb'))
+		for book in l.keys():
+			senti_sum = sum(l[book]['pol'])
+			if len(l[book]['pol'])!=0:
+				ranks[book][week]['avg_sent'] = senti_sum/len(l[book]['pol'])
 			else:
-				running_sum += -1*score[1]
+				ranks[book][week]['avg_sent'] = 0
+			
+			p, n = 0, 0
+			cues = l[book]['cue']
+			for c in cues:
+				p +=c[0]
+				n +=c[1]
+			ranks[book][week]['cue'] = p+n
+			if len(cues) !=0:
+				ranks[book][week]['p_cue'] = p/len(cues)
+				ranks[book][week]['n_cue'] = n/len(cues)
+			else:
+				ranks[book][week]['p_cue'] = 0
+				ranks[book][week]['n_cue'] = 0
 
-		return running_sum
+			e_dict = defaultdict(int)
+			for d in l[book]['emo']:
+				for k, v in d.iteritems():
+					e_dict[k] += v
+			ranks[book][week]['emo'] = e_dict
+
+			
+			pmi_sum = sum(l[book]['pmi'])
+			if len(l[book]['pmi'])!=0:
+				ranks[book][week]['pmi'] = pmi_sum/len(l[book]['pmi'])
+			else:
+				ranks[book][week]['pmi'] = 0
 
 
-	def _evaluate_emoticons(self, text):
-		"""
-		Get rid of emoticons in tweet and convert emoticons to score. A score
-		is a list object [posScore, negScore]
-		"""
-		score = [0,0]
 
-		return score, text
 
-	def _lookup_pos(pos):
-		"""
-		Convert POS to one of {'a', 's', 'n', 'r', 'v'} so sentiwordnet can
-		score it. Note: n - NOUN, v - VERB, a - ADJECTIVE, s - ADJECTIVE
-		SATELLITE, r - ADVERB 
 
-		Note that Stanford POSTagger uses the Penn TreeBank tag set. Refer to
-		this for a comprehensive list: http://www.computing.dcu.ie/~acahill/tagset.html
-		"""
-		if not (pos in pos_table):
-			return 'q'
 
-		return pos_table[pos]
 
-	def rank(self, pairs):
-		"""
-		Argument:
-		pairs -- A list of (book, tweets) tuples. book should be a book title(string) and tweets should be a list of tweets (class tweet)
 
-		Return:
-		ranking
-		"""		
-		scoreboard = {}
-		for item in pairs:
-			scoreboard[item[0]] = compute_score(item[1])
-		from operator import itemgetter
-		ranking = sorted(scoreboard.items(), key=itemgetter(1), reverse=True)
-		# return [t[0] for t in ranking]
-		return ranking
 
